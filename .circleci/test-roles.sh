353695b59578e484493718373f3fa7dfe8bc5817
Nathaniel Beckstead <becksteadn@gmail.com>
2019-06-08 13:32:15 -0400

    fail but keep testing

--- .circleci/test-roles.sh
+++ .circleci/test-roles.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-LINES="=========================================================================================================="
 PROJECT_DIR=$(pwd)
 LATEST_COMMIT=$(git rev-parse HEAD)
+FORCE=false # Test $TEST_ROLE role whether its changed or not
+TEST_ROLE="mail" # Name of role to test
+FAIL=0
 
 for D in roles/*/*
 do
     ROLE_COMMIT=$(git log -1 --format=format:%H --full-diff $D)
-    if [ $ROLE_COMMIT = $LATEST_COMMIT ]
+    if ([ $FORCE == true ] && [ $(basename $D) == "$TEST_ROLE" ]) || [ "$ROLE_COMMIT" == "$LATEST_COMMIT" ]
         then
             echo "Testing $D..."
             cd $D
 
-            molecule lint || exit 0
-            molecule syntax || exit 0
-            molecule converge -s docker
-            molecule idempotence -s docker || exit 0
-            molecule verify -s docker
-            molecule destroy -s docker
+            molecule lint || FAIL=2
+            molecule syntax || FAIL=2
+            molecule converge -s docker || FAIL=2
+            molecule idempotence -s docker || FAIL=2
+            molecule verify -s docker || FAIL=2
+            molecule destroy -s docker || FAIL=2
 
             cd $PROJECT_DIR
-            echo "\n\n\n$LINES"
+
+            LINES="=========================================================================================================="
+            echo -e "\n\n\n$LINES"
         else
             echo "Role $D not changed since last test. Skipping..."
     fi
 done
+
+echo $FAIL
+exit $FAIL
